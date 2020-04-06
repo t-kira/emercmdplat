5d8874072967c83974715bc133f667305788668d
t-kira <lucybanwei@126.com>
2020-04-07 00:57:02 +0800

    Update EmergencyFundController.java

--- src/main/java/com/kira/emercmdplat/controller/EmergencyFundController.java
+++ src/main/java/com/kira/emercmdplat/controller/EmergencyFundController.java
@@ -26,35 +26,35 @@ public class EmergencyFundController {
 
     @RequestMapping("/add")
     public AlvesJSONResult insert(EmergencyFund emergencyFund) {
-    	emergencyFundService.insert(emergencyFund);
+        emergencyFundService.insert(emergencyFund);
         return AlvesJSONResult.ok();
     }
-    
+
     @RequestMapping("/update")
     public AlvesJSONResult update(EmergencyFund emergencyFund) {
-    	emergencyFundService.update(emergencyFund);
+        emergencyFundService.update(emergencyFund);
         return AlvesJSONResult.ok();
     }
-    
+
     @RequestMapping("/delete")
     public AlvesJSONResult delete(EmergencyFund emergencyFund) {
-    	emergencyFundService.delete(emergencyFund);
+        emergencyFundService.delete(emergencyFund);
         return AlvesJSONResult.ok();
     }
-    
+
     @RequestMapping("/selectById")
     public AlvesJSONResult selectById(Integer id) {
-    	EmergencyFund emergencyFund = emergencyFundService.selectById(id);
+        EmergencyFund emergencyFund = emergencyFundService.selectById(id);
         return AlvesJSONResult.ok(emergencyFund);
     }
-    
+
     @RequestMapping("/list")
-    public AlvesJSONResult list(EmergencyFund emergencyFund,Integer page,Integer pageSize) {
-    	Map<String,Object> map = new HashMap<>();
-    	List<EmergencyFund> list = emergencyFundService.queryForPage(emergencyFund, page, pageSize);
-    	Long count = emergencyFundService.queryForCounts(emergencyFund);
-    	map.put("list", list);
-    	map.put("count", count);
+    public AlvesJSONResult list(EmergencyFund emergencyFund, Integer page, Integer pageSize) {
+        Map<String, Object> map = new HashMap<>();
+        List<EmergencyFund> list = emergencyFundService.queryForPage(emergencyFund, page, pageSize);
+        Long count = emergencyFundService.queryForCounts(emergencyFund);
+        map.put("list", list);
+        map.put("count", count);
         return AlvesJSONResult.ok(map);
     }
 }
