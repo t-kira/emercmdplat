b8d03129809f35d417ce6000fe65e75e40753163
t-kira <lucybanwei@126.com>
2020-04-07 00:57:19 +0800

    Update ProtectionTargetController.java

--- src/main/java/com/kira/emercmdplat/controller/ProtectionTargetController.java
+++ src/main/java/com/kira/emercmdplat/controller/ProtectionTargetController.java
@@ -26,35 +26,35 @@ public class ProtectionTargetController {
 
     @RequestMapping("/add")
     public AlvesJSONResult insert(ProtectionTarget protectionTarget) {
-    	protectionTargetService.insert(protectionTarget);
+        protectionTargetService.insert(protectionTarget);
         return AlvesJSONResult.ok();
     }
-    
+
     @RequestMapping("/update")
     public AlvesJSONResult update(ProtectionTarget protectionTarget) {
-    	protectionTargetService.update(protectionTarget);
+        protectionTargetService.update(protectionTarget);
         return AlvesJSONResult.ok();
     }
-    
+
     @RequestMapping("/delete")
     public AlvesJSONResult delete(ProtectionTarget protectionTarget) {
-    	protectionTargetService.delete(protectionTarget);
+        protectionTargetService.delete(protectionTarget);
         return AlvesJSONResult.ok();
     }
-    
+
     @RequestMapping("/selectById")
     public AlvesJSONResult selectById(Integer id) {
-    	ProtectionTarget protectionTarget = protectionTargetService.selectById(id);
+        ProtectionTarget protectionTarget = protectionTargetService.selectById(id);
         return AlvesJSONResult.ok(protectionTarget);
     }
-    
+
     @RequestMapping("/list")
-    public AlvesJSONResult list(ProtectionTarget protectionTarget,Integer page,Integer pageSize) {
-    	Map<String,Object> map = new HashMap<>();
-    	List<ProtectionTarget> list = protectionTargetService.queryForPage(protectionTarget, page, pageSize);
-    	Long count = protectionTargetService.queryForCounts(protectionTarget);
-    	map.put("list", list);
-    	map.put("count", count);
+    public AlvesJSONResult list(ProtectionTarget protectionTarget, Integer page, Integer pageSize) {
+        Map<String, Object> map = new HashMap<>();
+        List<ProtectionTarget> list = protectionTargetService.queryForPage(protectionTarget, page, pageSize);
+        Long count = protectionTargetService.queryForCounts(protectionTarget);
+        map.put("list", list);
+        map.put("count", count);
         return AlvesJSONResult.ok(map);
     }
 }
