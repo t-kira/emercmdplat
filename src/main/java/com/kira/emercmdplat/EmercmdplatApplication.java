df961423a987980b4dbc6330def75202bb58e350
t-kira <lucybanwei@126.com>
2020-04-07 00:47:07 +0800

    修改扫描包路径

--- src/main/java/com/kira/emercmdplat/EmercmdplatApplication.java
+++ src/main/java/com/kira/emercmdplat/EmercmdplatApplication.java
@@ -4,17 +4,16 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@MapperScan("com.kra.emercmdplat.mapper")
+@MapperScan("com.kira.emercmdplat.mapper")
 @ComponentScan(basePackages= {"com.kira", "com.kira.emercmdplat"})
 @EnableTransactionManagement//开启事务（默认是开启的）
 @SpringBootApplication
 public class EmercmdplatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmercmdplatApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmercmdplatApplication.class, args);
+    }
 
 }
