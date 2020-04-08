664d63691f1c5d3ab915b225fa66f7d5902c0085
t-kira <lucybanwei@126.com>
2020-04-08 23:19:39 +0800

    添加baseContoller

---
+++ src/main/java/com/kira/emercmdplat/controller/base/BaseController.java
@@ -0,0 +1,12 @@
+package com.kira.emercmdplat.controller.base;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+/**
+ * @Author: kira
+ * @Date: 2020/4/8 23:15
+ * @Description:
+ */
+@CrossOrigin
+public class BaseController {
+}
