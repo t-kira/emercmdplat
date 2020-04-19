8296aa52c327efa4f344f83661382b4214d0cd85
t-kira <lucybanwei@126.com>
2020-04-19 16:46:39 +0800

    add POJO

---
+++ src/main/java/com/kira/emercmdplat/pojo/ReservePlan.java
@@ -0,0 +1,18 @@
+package com.kira.emercmdplat.pojo;
+
+import lombok.Data;
+
+/**
+ * @Author: kira
+ * @Date: 2020/4/13 17:18
+ * @Description:
+ */
+@Data
+public class ReservePlan {
+
+    private Long id;
+
+    private Long eId;
+
+    private Integer status;
+}
---
+++ src/main/java/com/kira/emercmdplat/pojo/ReservePlanResult.java
@@ -0,0 +1,33 @@
+package com.kira.emercmdplat.pojo;
+
+import lombok.Data;
+
+/**
+ * @Author: kira
+ * @Date: 2020/4/13 17:20
+ * @Description:
+ */
+@Data
+public class ReservePlanResult extends ReservePlan{
+
+    /**
+     * 事件类型名称
+     */
+    private String ptName;
+    /**
+     * 初判预案ID
+     */
+    private Long pvId;
+    /**
+     * 初判预案名称
+     */
+    private String pvName;
+    /**
+     * 预案等级ID
+     */
+    private Long prId;
+    /**
+     * 核实报告ID
+     */
+    private Long vrId;
+}
