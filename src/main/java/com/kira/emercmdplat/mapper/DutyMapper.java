c317a3f27c5f59ddbd01a739d03aa3e094328efa
t-kira <lucybanwei@126.com>
2020-04-19 16:40:11 +0800

    修改扫描包路径

--- src/main/java/com/kira/emercmdplat/mapper/DutyMapper.java
+++ src/main/java/com/kira/emercmdplat/mapper/DutyMapper.java
@@ -2,11 +2,12 @@ package com.kira.emercmdplat.mapper;
 
 import com.kira.emercmdplat.mapper.base.BaseMapper;
 import com.kira.emercmdplat.pojo.Duty;
+import com.kira.emercmdplat.pojo.DutyExtent;
 
 /**
  * @Author: kira
  * @Date: 2020/4/6 13:59
  * @Description:
  */
-public interface DutyMapper extends BaseMapper<Duty> {
+public interface DutyMapper extends BaseMapper<DutyExtent> {
 }
