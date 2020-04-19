be079f22e8fca51f8a93161d40f2b9a07e248de6
t-kira <lucybanwei@126.com>
2020-04-19 16:45:41 +0800

    add pojo

---
+++ src/main/java/com/kira/emercmdplat/pojo/VerifyQuickReport.java
@@ -0,0 +1,16 @@
+package com.kira.emercmdplat.pojo;
+
+import lombok.Data;
+
+/**
+ * @Author: kira
+ * @Date: 2020/4/18 23:05
+ * @Description:
+ */
+@Data
+public class VerifyQuickReport {
+
+    private VerifyReport verify;
+
+    private QuickReport quick;
+}
--- src/main/java/com/kira/emercmdplat/pojo/VerifyReport.java
+++ src/main/java/com/kira/emercmdplat/pojo/VerifyReport.java
@@ -1,17 +1,20 @@
 package com.kira.emercmdplat.pojo;
 
+import lombok.Data;
+
 /**
  * @Author: kira
  * @Date: 2020/4/7 22:44
  * @Description:核实报告
  */
+@Data
 public class VerifyReport {
 
     private Long id;
     /**
      * event_receive.id事件ID
      */
-    private Long eId;
+    private Long eid;
     /**
      * plan_version.id
      */
@@ -20,6 +23,10 @@ public class VerifyReport {
      * plan_response.id
      */
     private Long prId;
+    /**
+     * 领导ID
+     */
+    private Long did;
     /**
      * 研判意见
      */
@@ -32,60 +39,4 @@ public class VerifyReport {
      * 创建时间
      */
     private String createTime;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long geteId() {
-        return eId;
-    }
-
-    public void seteId(Long eId) {
-        this.eId = eId;
-    }
-
-    public Long getPvId() {
-        return pvId;
-    }
-
-    public void setPvId(Long pvId) {
-        this.pvId = pvId;
-    }
-
-    public Long getPrId() {
-        return prId;
-    }
-
-    public void setPrId(Long prId) {
-        this.prId = prId;
-    }
-
-    public String getJudgeOpinion() {
-        return judgeOpinion;
-    }
-
-    public void setJudgeOpinion(String judgeOpinion) {
-        this.judgeOpinion = judgeOpinion;
-    }
-
-    public String getAttachAddress() {
-        return attachAddress;
-    }
-
-    public void setAttachAddress(String attachAddress) {
-        this.attachAddress = attachAddress;
-    }
-
-    public String getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime;
-    }
 }
