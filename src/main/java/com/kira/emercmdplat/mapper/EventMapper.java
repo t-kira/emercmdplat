06325f606311f0bb2e6f32fe334984cda40405be
t-kira <lucybanwei@126.com>
2020-04-19 16:44:53 +0800

    fix bugs

--- src/main/java/com/kira/emercmdplat/mapper/EventMapper.java
+++ src/main/java/com/kira/emercmdplat/mapper/EventMapper.java
@@ -1,20 +1,37 @@
 package com.kira.emercmdplat.mapper;
 
 import com.kira.emercmdplat.mapper.base.BaseMapper;
-import com.kira.emercmdplat.pojo.Event;
-import com.kira.emercmdplat.pojo.EventDevelopment;
-import com.kira.emercmdplat.pojo.EventParam;
+import com.kira.emercmdplat.pojo.*;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: kira
  * @Date: 2020/4/6 22:46
  * @Description:
  */
-public interface EventMapper extends BaseMapper<Event> {
+public interface EventMapper{
+
+    int insert(Event event);
+
+    boolean delete(Event event);
+
+    boolean update(Event event);
+
+    EventResult selectById(Long id);
+
+    EventResult selectByEId(Long eId);
+
+    List<EventResult> queryForAll(EventExtend eventExtend);
+
+    Long queryForCounts(EventExtend eventExtend);
 
     int insertParam(EventParam eventParam);
 
     int insertDevelopment(EventDevelopment eventDevelopment);
 
     int updateDevelopment(EventDevelopment eventDevelopment);
+
+    List<EventResult> queryForPage(EventExtend eventExtend);
 }
