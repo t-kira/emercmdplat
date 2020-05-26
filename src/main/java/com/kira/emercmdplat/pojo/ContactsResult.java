56e51fe125f3ec421db8db4aca5795b7c520785e
t-kira <lucybanwei@126.com>
2020-05-26 11:25:30 +0800

    Update ContactsResult.java

--- src/main/java/com/kira/emercmdplat/pojo/ContactsResult.java
+++ src/main/java/com/kira/emercmdplat/pojo/ContactsResult.java
@@ -1,10 +1,13 @@
 package com.kira.emercmdplat.pojo;
 
+import lombok.Data;
+
 /**
  * @Author: kira
  * @Date: 2020/4/21 22:51
  * @Description:
  */
+@Data
 public class ContactsResult extends Contacts{
 
     private String jName;
