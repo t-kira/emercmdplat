e645b532651808588edaa5fcb13fc0b5f1b3ee67
t-kira <lucybanwei@126.com>
2020-06-01 10:20:20 +0800

    add login

---
+++ src/main/java/com/kira/emercmdplat/config/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package com.kira.emercmdplat.config;
+
+/**
+ * @Author: kira
+ * @Date: 2020/6/1 10:14
+ * @Description:
+ */
+public interface WebSecurityConfig {
+
+    String SESSION_KEY = "1a6ac48801fd8d5adae8761e14574d74";
+}
--- src/main/java/com/kira/emercmdplat/controller/LoginController.java
+++ src/main/java/com/kira/emercmdplat/controller/LoginController.java
@@ -2,12 +2,10 @@ package com.kira.emercmdplat.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.kira.emercmdplat.config.WebSecurityConfig;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.kira.emercmdplat.pojo.Contacts;
 import com.kira.emercmdplat.pojo.ContactsResult;
@@ -16,12 +14,11 @@ import com.kira.emercmdplat.utils.AlvesJSONResult;
 import com.kira.emercmdplat.utils.MD52;
 
 @RestController
-@RequestMapping("/login")
 public class LoginController {
 
 	@Autowired
 	private ContactService contactService;
-	
+
     @PostMapping(value = "login")
 	public AlvesJSONResult login(@RequestBody Contacts contacts,HttpServletRequest request) {
     	if (null == contacts.getUsername() || null == contacts.getPassword()) {
@@ -34,10 +31,15 @@ public class LoginController {
     	String md5password = user.getPassword();
     	String _pwd = MD52.MD5Encode(contacts.getPassword());
     	if (StringUtils.equals(md5password, _pwd)) {
-    		request.getSession().setAttribute("user", user);
+    		request.getSession().setAttribute(WebSecurityConfig.SESSION_KEY, user);
     		return AlvesJSONResult.ok(user);
     	} else {
     		return AlvesJSONResult.errorMsg("密码错误");
     	}
     }
+
+	@GetMapping("/login_page")
+	public String login(){
+		return "login";
+	}
 }
