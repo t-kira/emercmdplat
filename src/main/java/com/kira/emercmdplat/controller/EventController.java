bd7ac53a93bb52173a46e177a9634d8064836a07
richard.yuq <richard_yuq@163.com>
2020-06-07 18:19:07 +0800

    bugfix

--- src/main/java/com/kira/emercmdplat/controller/EventController.java
+++ src/main/java/com/kira/emercmdplat/controller/EventController.java
@@ -262,6 +262,7 @@ public class EventController extends BaseController {
     public AlvesJSONResult listPlanVersion(@PathVariable int id) {
         PlanVersion planVersion = new PlanVersion();
         planVersion.setType(id);
+        planVersion.setStatus(4);
         List<PlanVersion> planVersionList = pvs.listAllVersions(planVersion);
         return AlvesJSONResult.ok(planVersionList);
     }
