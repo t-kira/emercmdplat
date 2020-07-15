6468b16f3391f89e8bc701c81ca1911b7ebc6f0e
richard.yuq <richard_yuq@163.com>
2020-07-15 10:37:22 +0800

    bugfix

--- src/main/java/com/kira/emercmdplat/service/PlanTypeService.java
+++ src/main/java/com/kira/emercmdplat/service/PlanTypeService.java
@@ -16,7 +16,7 @@ public interface PlanTypeService {
 
     void updateType(PlanType planType);
 
-    void deleteType(Integer id);
+    boolean deleteType(Integer id);
 
     List<PlanTag> listTags(int ptId,Boolean includeCommon);
 
@@ -59,5 +59,7 @@ public interface PlanTypeService {
 	PlanType getPlanTypeById(Integer id);
 
 	List<PlanParam> getParamByJson(String params);
+	
+	Long countsForParentId(Integer id);
 
 }
